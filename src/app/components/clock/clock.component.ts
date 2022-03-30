bb402ea77c8d6ee3c14d36a7723b46fbca1cdefe
cindyIsa <onnea666@gmail.com>
2022-03-30 18:35:27 +0300

    add bg and fix clock size

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Observable, Observer } from 'rxjs';
 
 @Component({
@@ -7,22 +7,25 @@ import { Observable, Observer } from 'rxjs';
   styleUrls: ['./clock.component.scss']
 })
 export class ClockComponent implements OnInit {
+  @Input() clockSize = 200 
   hourDeg = {}
   minuteDeg = {}
   secondDeg = {}
   time = Array(12).fill(1).map((a,b) => a +b)
+  clockSizeStyle ={}
   constructor() { }
 
   ngOnInit(): void {
-    console.log( new  Date() )
+    this.clockSizeStyle = { 'width':this.clockSize + 'px', 'height':this.clockSize + 'px' }
     this.getDatePerSecond().subscribe(
       (res) => {         
         const sdeg = res.second*6 + 'deg'
         const mdeg = res.minute*6 + 'deg'
-        const hdeg = res.hour*24/360 + res.minute*30/60  + 'deg'
-        this.secondDeg = {'transform': 'rotate('+sdeg+') translateY(-50%)' } 
-        this.minuteDeg = {'transform': 'rotate('+mdeg+') translateY(-50%) translateX(5%)' } 
-        this.hourDeg = {'transform': 'rotate('+hdeg+') translateY(-50%) translateX(5%)' } 
+        const hdeg = res.hour*360/12 + res.minute*30/60 + 'deg'
+        console.log(res.hour,hdeg)
+        this.secondDeg = {'transform': 'translateY(-100%)  rotate('+sdeg+')' } 
+        this.minuteDeg = {'transform': 'translateY(-100%)  rotate('+mdeg+') ' } 
+        this.hourDeg = {'transform': 'translateY(-100%)  rotate('+hdeg+') ' } 
       }
     )
   }
@@ -34,35 +37,5 @@ export class ClockComponent implements OnInit {
       },1000)
      })
   }
-  position= (num:number) => { 
-    if(num == 12){
-      return {'top':'0px'}
-    }else if(num === 6){
-      return {'bottom':'0'}
-    }else if(num === 9){
-      return {'left':'0px'}
-    
-    }else if(num === 3){
-      return {'right':'0px'}
-    
-    }else if(num > 6){
-      const top =150 - 150*Math.cos(Math.abs(12-num)*30*Math.PI/180) + 'px'
-      const left = 150 - 150*Math.cos(Math.abs(num-9)*30*Math.PI/180) + 'px'
-      if(num < 9){
-        const bottom =150 - 150*Math.cos(Math.abs(6-num)*30*Math.PI/180) + 'px'
-        return {'bottom':bottom, 'left':left} 
-      }
-      return {'top':top, 'left':left}      
-    }else{
-      const bottom =150 - 150*Math.cos(Math.abs(6-num)*30*Math.PI/180) + 'px'
-      const right = 150 - 150*Math.cos(Math.abs(num-3)*30*Math.PI/180) + 'px'
-      if(num < 3){
-        const top =150 - 150*Math.cos(Math.abs(12-num)*30*Math.PI/180) + 'px'
-        return {'top':top, 'right':right} 
-      }
-      return {'bottom':bottom, 'right':right} 
-    }
-    return null
-   }
-
+  
 }
